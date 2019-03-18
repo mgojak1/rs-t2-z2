acc145955980e5fcc14cf79689c299b15aab3976
Mirza Gojak <mgojak1@etf.unsa.ba>
2019-03-18 10:11:51 +0100

    Zadatak 2

--- src/ba/unsa/etf/rs/Main.java
+++ src/ba/unsa/etf/rs/Main.java
@@ -4,18 +4,21 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static int sumaCifara(int n){
+        int suma = 0;
+        while (n != 0){
+            suma = suma + (n%10);
+            n = n/10;
+        }
+        return suma;
+    }
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Unesite broj n: ");
         int n = s.nextInt();
         for (int i = 1; i < n;i++){
-            int suma = 0;
-            int temp = i;
-            while (temp != 0){
-                suma = suma + (temp%10);
-                temp = temp/10;
-            }
-            if(i % suma == 0) System.out.println(i);
+            if(i % sumaCifara(i) == 0) System.out.println(i);
         }
     }
 }
