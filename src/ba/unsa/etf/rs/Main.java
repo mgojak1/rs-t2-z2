f60682ab4122903c997ee6fd64379841f02b8491
Mirza Gojak <mgojak1@etf.unsa.ba>
2019-03-18 09:55:59 +0100

    Zadatak 2

--- src/ba/unsa/etf/rs/Main.java
+++ src/ba/unsa/etf/rs/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.print("Unesite broj n: ");
         int n = s.nextInt();
         for (int i = 1; i < n;i++){
             int suma = 0;
