44816faefef87e63090017c7f139fb788c209e58
Mirza Gojak <mgojak1@etf.unsa.ba>
2019-03-18 09:53:20 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rs/Main.java
@@ -0,0 +1,20 @@
+package ba.unsa.etf.rs;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for (int i = 1; i < n;i++){
+            int suma = 0;
+            int temp = i;
+            while (temp != 0){
+                suma = suma + (temp%10);
+                temp = temp/10;
+            }
+            if(i % suma == 0) System.out.println(i);
+        }
+    }
+}
